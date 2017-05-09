9894d62312d2eacb261156265a6d153559d47f2d
Raim Interiano <rai.interiano@gmail.com>
2017-05-08 19:53:39 -0600

    Parser finished with basic CFG for read, print, and assign methods

--- Compiler/Parser.cs
+++ Compiler/Parser.cs
@@ -1,4 +1,6 @@
-﻿namespace Compiler
+﻿using System.Data;
+
+namespace Compiler
 {
     public class Parser
     {
@@ -13,12 +15,114 @@
 
         public void Parse()
         {
-            E();
+            Codigo();
             if (token.type != TokenType.EOF)
             {
                 throw new ParserException("End of file expected.");
             }
+        }
 
+        private void Codigo()
+        {
+            ListaSentencias();
+        }
+
+        private void ListaSentencias()
+        {
+            if (
+                token.type == TokenType.ID ||
+                token.type == TokenType.PRINT_CALL ||
+                token.type == TokenType.READ_CALL
+            )
+            {
+                Sentencia();
+                ListaSentencias();
+            }
+            else
+            {
+                //TODO: Epsilon case 
+            }
+        }
+
+        private void Sentencia()
+        {
+            if (token.type == TokenType.ID)
+            {
+                Asignar();
+            }
+            else if (token.type == TokenType.PRINT_CALL)
+            {
+                Imprimir();
+            }
+            else if (token.type == TokenType.READ_CALL)
+            {
+                Leer();
+            }
+            else
+            {
+                throw new SyntaxErrorException("Sentencia expected on row " +
+                    token.row + " and column " + token.column);
+            }
+        }
+
+        private void Leer()
+        {
+            if (token.type != TokenType.READ_CALL)
+            {
+                throw new SyntaxErrorException("read token type expected on row " +
+                    token.row + " and column " + token.column);
+            }
+            token = lexer.GetNextToken();
+            if (token.type != TokenType.ID)
+            {
+                throw new SyntaxErrorException("ID token type expected on row " +
+                    token.row + " and column " + token.column);
+            }
+            token = lexer.GetNextToken();
+            if (token.type != TokenType.END_STATEMENT)
+            {
+                throw new SyntaxErrorException("; operand expected on row " +
+                    token.row + " and column " + token.column);
+            }
+            token = lexer.GetNextToken();
+        }
+
+        private void Imprimir()
+        {
+            if (token.type != TokenType.PRINT_CALL)
+            {
+                throw new SyntaxErrorException("PRINT token type expected on row " + 
+                    token.row + " and column " + token.column);
+            }
+            token = lexer.GetNextToken();
+            E();
+            if (token.type != TokenType.END_STATEMENT)
+            {
+                throw new SyntaxErrorException("; operand expected on row " +
+                    token.row + " and column " + token.column);
+            }
+            token = lexer.GetNextToken();
+        }
+
+        private void Asignar()
+        {
+            if (token.type != TokenType.ID)
+            {
+                throw new SyntaxErrorException("ID Token Type expected!");
+            }
+            token = lexer.GetNextToken();
+            if (token.type != TokenType.OP_ASSIGN)
+            {
+                throw new SyntaxErrorException("= operand expected!");
+            }
+            token = lexer.GetNextToken();
+            E();
+            
+            if (token.type != TokenType.END_STATEMENT)
+            {
+                throw new SyntaxErrorException("; operand expected");
+            }
+            token = lexer.GetNextToken();
         }
 
         private void E()
--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -10,33 +11,14 @@ namespace Compiler
     {
         static void Main(string[] args)
         {
-            var inputString = new InputString(@"5+5-(9+5)");
+            var inputString = new InputString(@"read a; read b; c = a + b; print (a+b+c);");
 
             var lexer = new Lexer(inputString);
-
-            /*Token token = lexer.GetNextToken();
-
-            while (token.type != TokenType.EOF)
-            {
-                System.Console.Out.WriteLine(token);
-                token = lexer.GetNextToken();
-            }*/
+            
 
             var parser = new Parser(lexer);
             parser.Parse();
-            //System.Console.Out.WriteLine(token);
-            
-            //Symbol currentSymbol = inputString.GetNextSymbol();
-
-        
-            /*while (currentSymbol.character != '\0')
-            {
-                Console.WriteLine("Row: " + currentSymbol.rowCount +
-                    " Column: " + currentSymbol.colCount + 
-                    " Char: " + currentSymbol.character);
-                currentSymbol = inputString.GetNextSymbol();
-            }*/
-
+            Console.Out.WriteLine("EXIT!!");
             System.Console.ReadKey();
         }
     }
